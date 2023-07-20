e77719a64f882b52ea62a2b3ec4fbb2cd6f5da35
sooyon lee <kwlsy666@naver.com>
2023-07-20 15:12:21 +0900

    react 오프라인 2강 - 실행흐름이해하기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
+//0. React 엔진 - 데이터 변경 감지에서 UI그려주는 !!
+//1. 실행방식(index.html) -SPA
+//2. JSX 문법
+//3. 바벨(자바스크립트 ES5) -> ES6
 function App() {
-  return <div>안녕</div>;
+  return <div>안녕1</div>;
 }
 
 export default App;
