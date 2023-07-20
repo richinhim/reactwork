4dad8665bdc1a20cca2a7aa9116c0208a4489cf1
sooyon lee <kwlsy666@naver.com>
2023-07-20 20:06:27 +0900

    react 오프라인 4강 - 배열(map, filter, slice, concat, spread연산자)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-//0. React 엔진 - 데이터 변경 감지에서 UI그려주는 !!
-//1. 실행방식(index.html) -SPA
-//2. JSX 문법
-//3. 바벨(자바스크립트 ES5) -> ES6
-
-// (1) return시에 하나의 DOM만 리턴할 수 있다.
-// (2) 변수선언은 let 혹은 const로만 해야함.
-// (3) if사용불가능 X -> 삼항연산자 (조건 ? 값(true):값(false))
-// (4) 조건부 렌더링(조건 && 값(true))
-// (5) css디자인
-//     - 내부에 적는 방법
-//     - 외부파일에 적는 방법
-//     - 라이브러리 사용(부트스트랩,component-styled)
-
-let a = 10; // 변수
-const b = 20; // 상수
-
 function App() {
-  let c;
-  let d = undefined;
-  console.log(1, c);
-
-  const mystyle = {
-    color: 'red',
-  };
+  let list = [1, 2, 3];
 
   return (
     <div>
-      <div style={mystyle}>안녕 {a === 10 ? '10입니다.' : ''}</div>
-      <h1 className="box-style">해딩태그 {b === 20 && '20입니다.'}</h1>
-      <hr />
+      <div>
+        {list.map((n) => (
+          <h1>{n}</h1>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ my-app/src/wood.js
@@ -0,0 +1,9 @@
+function Wood() {
+  return (
+    <div>
+      <div>나무</div>
+    </div>
+  );
+}
+
+export default Wood;
