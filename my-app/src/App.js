d5095ed9809b053426bdba646dfcb9f4d566f265
sooyon lee <kwlsy666@naver.com>
2023-07-22 19:29:45 +0900

    1.react 오프라인 6강 - useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Sub from './Sub';
 
 function App() {
-  let list = [1, 2, 3];
+  // let number = 1; //상
+  const [number, setNumber] = useState(2); // React안에 hooks 라이브러리 상태값이 됨.
 
+  const add = () => {
+    //number++;
+    setNumber(number + 1); // 리엑트한테 number 값 변경할께라고 요청
+    console.log('add', number);
+  };
+
+  //랜더링 시점 = 상태값 변경
   return (
     <div>
-      <div>
-        {list.map((n) => (
-          <h1>{n}</h1>
-        ))}
-      </div>
+      <h1>숫자: {number}</h1>
+      <button onClick={add}>더하기</button>
+      <Sub />
     </div>
   );
 }
