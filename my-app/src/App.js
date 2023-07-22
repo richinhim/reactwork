6a9ad857ed72a7592efec5d1d4bcd41266346791
sooyon lee <kwlsy666@naver.com>
2023-07-22 20:08:18 +0900

    react 오프라인 6강 - useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,40 @@
-import { useState } from 'react';
 import './App.css';
-import Sub from './Sub';
+import { useState } from 'react';
 
 function App() {
-  // let number = 1; //상
-  const [number, setNumber] = useState(2); // React안에 hooks 라이브러리 상태값이 됨.
+  // 다운로드 받음
 
-  const add = () => {
-    //number++;
-    setNumber(number + 1); // 리엑트한테 number 값 변경할께라고 요청
-    console.log('add', number);
+  console.log('APP 실행됨');
+
+  const [num, setNum] = useState(5);
+
+  let sample = [
+    { id: 1, name: '홍길동' },
+    { id: 2, name: '임꺽정' },
+    { id: 3, name: '장보고' },
+    { id: 4, name: '코스' },
+  ];
+  const [users, setUsers] = useState(sample); //레퍼런스 변경되야 동작!!
+
+  const download = () => {
+    //fetch().then().then();
+    //기존 데이터 세팅
+    //const a = sample.concat({ id: 5, name: '조자룡' });
+    //console.log(sample);
+
+    setUsers([...sample, { id: num, name: '조자룡' }]);
+    //num++;
+    setNum(num + 1);
   };
-
   //랜더링 시점 = 상태값 변경
   return (
     <div>
-      <h1>숫자: {number}</h1>
-      <button onClick={add}>더하기</button>
-      <Sub />
+      <button onClick={download}>다운로드</button>
+      {users.map((u) => (
+        <h1>
+          {u.id},{u.name}
+        </h1>
+      ))}
     </div>
   );
 }
