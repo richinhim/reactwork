e1d506711332a9a97d17464ac53587d35a084cb8
sooyon lee <kwlsy666@naver.com>
2023-07-22 21:56:30 +0900

    react 오프라인 7강 - useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,8 @@ import { useEffect, useState } from 'react';
 function App() {
   const [data, setData] = useState(0);
 
+  const [search, setSearch] = useState(0);
+
   const download = () => {
     // 다운로드 받고 (통신)
     let downloadData = 5; // 가정
@@ -17,16 +19,23 @@ function App() {
   // (1) App()함수가 최초 실행될 때(마운트될 때, App() 그림이 최초 그려질 때) - 그림이 그려질때
 
   // (1) App() 그림이 최초 그려질 때
-  // (2) 상태 별수가 변경될 때
-  //
+  // (2) 상태 별수가 변경될 때(그게 dependencyList에 등록되어 있어야 함.)
+  // (3) 의존리스트 관리를 할 수 있다.
   useEffect(() => {
     console.log('useEffect 실행됨');
 
     download();
-  }, []);
+  }, [search]);
 
   return (
     <div>
+      <button
+        onClick={() => {
+          setSearch(2);
+        }}
+      >
+        검색하기
+      </button>
       <h1> 데이터: {data}</h1>
       <button
         onClick={() => {
